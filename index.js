6e2f0b3fc6cf5e22eb0282b8603070adc7a8d118
Justin Searls <searls@gmail.com>
2019-05-07 19:47:33 -0400

    work on the tests

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 const probotScheduler = require('probot-scheduler')
+const labelOfTheDay = require('./lib/label-of-the-day')
+// require('nock').recorder.rec({ gunzip: true })
 
 module.exports = app => {
   probotScheduler(app, {
@@ -7,7 +9,7 @@ module.exports = app => {
   app.on('schedule.repository', async function (context) {
     const github = context.github
     const { owner, repo } = context.repo({ logger: app.log })
-    const labelName = todaysLabelName()
+    const labelName = labelOfTheDay()
     app.log.debug(`Searching for open PRs for ${owner}/${repo} with label ${labelName}`)
     const label = await github.issues.getLabel({
       owner, repo, name: labelName
@@ -22,11 +24,13 @@ module.exports = app => {
 
       pulls.data.forEach(pull => {
         app.log.debug(`Merging PR: ${pull.url}`)
+
         github.pulls.merge({
           owner,
           repo,
           pull_number: pull.number
         }).catch(e => {
+          console.log('WTF', e)
           github.issues.createComment({
             owner,
             repo,
@@ -37,17 +41,4 @@ module.exports = app => {
       })
     }
   })
-}
-
-// Ripped right off SO:
-// https://stackoverflow.com/questions/23593052/format-javascript-date-to-yyyy-mm-dd
-function todaysLabelName () {
-  const d = new Date()
-  let month = String(d.getMonth() + 1)
-  let day = String(d.getDate())
-
-  if (month.length < 2) month = '0' + month
-  if (day.length < 2) day = '0' + day
-
-  return `merge-${d.getFullYear()}-${month}-${day}`
 }
--- test/index.test.js
+++ test/index.test.js
@@ -1,13 +1,15 @@
 const { Probot } = require('probot')
+const labelOfTheDay = require('../lib/label-of-the-day')
 
 describe('scheduled-merge', () => {
-  let probot, probotScheduler, app, api
+  let probot, probotScheduler, app, api, label
 
   beforeEach(() => {
     api = nock('https://api.github.com')
     probot = new Probot({
       githubToken: 'test'
     })
+    label = labelOfTheDay()
 
     probotScheduler = td.replace('probot-scheduler')
     app = probot.load(require('..'))
@@ -20,9 +22,11 @@ describe('scheduled-merge', () => {
   })
 
   test('does nothing when no label is found', async () => {
-    api.filteringPath(/\d{4}-\d{2}-\d{2}/g, 'YYYY-MM-DD')
-      .get('/repos/fake/stuff/labels/merge-YYYY-MM-DD')
-      .reply(404)
+    api.get(`/repos/fake/stuff/labels/${label}`)
+      .reply(404, {
+        message: 'Not Found',
+        documentation_url: 'https://developer.github.com/v3/issues/labels/#get-a-single-label'
+      })
 
     await probot.receive({ name: 'schedule.repository',
       payload: {
@@ -34,5 +38,52 @@ describe('scheduled-merge', () => {
         }
       }
     })
+  })
+
+  test('does nothing when label is found but no pulls are open', async () => {
+    api.get(`/repos/fake/stuff/labels/${label}`).reply(200, { name: label })
+
+    api.get('/repos/fake/stuff/issues')
+      .query({ 'labels': label, 'state': 'open' })
+      .reply(200, [])
+
+    await probot.receive({ name: 'schedule.repository',
+      payload: {
+        'repository': {
+          'name': 'stuff',
+          'owner': {
+            'login': 'fake'
+          }
+        }
+      }
+    })
+  })
+
+  test('merges PR when labeled, open, and mergeable', async () => {
+    api.get(`/repos/fake/stuff/labels/${label}`).reply(200, { name: label })
+
+    api.get('/repos/fake/stuff/issues')
+      .query({ 'labels': label, 'state': 'open' })
+      .reply(200, [{
+        url: 'fake pull url',
+        number: 999,
+        labels: [{ name: label }],
+        state: 'open'
+      }])
+
+    api.put('/repos/fake/stuff/pulls/999/merge').reply(200)
+
+    await probot.receive({ name: 'schedule.repository',
+      payload: {
+        'repository': {
+          'name': 'stuff',
+          'owner': {
+            'login': 'fake'
+          }
+        }
+      }
+    })
+
+    console.log('wat', nock.pendingMocks())
   })
 })
